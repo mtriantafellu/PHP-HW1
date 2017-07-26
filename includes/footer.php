6510b045ea49af8b11cc624fa80f013223fdd601
matt <mtriantafellu@yahoo.com>
2017-07-25 22:05:58 -0400

    cannot get links to work

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<div>
+    <div class="two-toned-footer-color"></div>
+    <p class="text-muted text-muted-footer text-center">
+        &copy; Copyright Triantafellu UCF Coding Boot Camp Summer 2017
+    </p>
+</div>
